c19f8ff93abf558a2db6d4908c889c8be57f8f53
Andrey Lysenkov <andreylysenkovallan@gmail.com>
2016-09-06 16:16:34 +0300

    [prog.lab.1] add Print to class

---
+++ programming/lab01/src/lab01.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : lab01.cpp
+// Author      : Andrey Lysenkov
+// Version     :
+// Copyright   : Walpy (Anrey Lysenkov)
+// Description : Hello World in C++, Ansi-style
+// Lab number  : 01
+// Lab variant : 07
+//============================================================================
+
+#include <iostream>
+#include "library.h"
+
+int main() {
+	Library lib = new Library();
+	lib.Print();
+
+
+	return 0;
+}
